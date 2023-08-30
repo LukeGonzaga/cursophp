6b0ca050d266effc56f21f85e8155f1dc3f70506
Lucas Gonzaga <lucassoft@gmx.com>
2023-08-30 02:43:39 -0300

    Initial commit

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro código PHP</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Olá, Mundo! \u{1f30e}";
+        ?>
+    </h1>
+    <p>Vamos começar</p>
+</body>
+</html>
