9ba26fb3c107a84294011419078235fdeaaf354c
Lucas Gonzaga <lucassoft@gmx.com>
2023-09-01 02:46:23 -0300

    Create index.php

---
+++ exex/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exemplo PHP</title>
+</head>
+<body>
+    <h1>Exemplo PHP</h1>
+    <?php 
+    $nome = "Lucas Gonzaga";
+    echo "<p>O meu nome é $nome</p>";
+    ?>   
+</body>
+</html>
